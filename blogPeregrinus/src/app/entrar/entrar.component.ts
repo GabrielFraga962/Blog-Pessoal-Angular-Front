ab3d170dfe2089abdf3c657e794853d32b9ad841
Gabriel S. Fraga <gabrielsouzafraga@outlook.com>
2021-04-09 18:14:33 -0300

    Ajuste finais concluidos

--- blogPeregrinus/src/app/entrar/entrar.component.ts
+++ blogPeregrinus/src/app/entrar/entrar.component.ts
@@ -32,6 +32,7 @@ export class EntrarComponent implements OnInit {
       environment.nome = this.userLogin.nome
       environment.foto = this.userLogin.foto
       environment.id = this.userLogin.id
+      environment.tipo = this.userLogin.tipo
 
       //Caso eu queira mostrar no console do inspecionar (opcional)
       console.log(environment.token)
